a2cec10ae5cea66c56560bcac85a5dc410889fdc
Raymon Pichardo <raymon2000@hotmail.com>
2015-05-19 23:15:33 -0430

    1st changeset

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using RationalNumbers.Models;
+using System.IO;
+
+namespace RationalNumbers.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        public ActionResult Index(string str)
+        {
+            try
+            {
+                string input, add, sub, mul, div;
+                List<Rational> rationals = new List<Rational>();
+                Rational rat1, rat2;
+                string r1, r2;
+ 
+                input = Request["txtInput"].ToString().TrimStart().TrimEnd();    
+                
+                rationals = splitString(input, 2);
+
+                rat1 = rationals[0];
+                rat2 = rationals[1];
+                
+                add = rat1.add(rat2).Simplify();
+                sub = rat1.sub(rat2).Simplify();
+                mul = rat1.mul(rat2).Simplify();
+                div = rat1.div(rat2).Simplify();
+
+                r1 = rat1.Simplify();
+                r2 = rat2.Simplify();
+
+                if(add != "Inf")
+                    add = r1 + " + " + r2 + " = " + add;
+
+                if (sub != "Inf")
+                    sub = r1 + " - " + r2 + " = " + sub;
+
+                if (mul != "Inf") 
+                    mul = r1 + " * " + r2 + " = " + mul;
+
+                if (div != "Inf") 
+                    div = r1 + " / " + r2 + " = " + div;
+
+                ViewData["Result"] = add + "\r\n" + sub + "\r\n" + mul + "\r\n" + div;                               
+
+            }
+            catch (Exception e)
+            {
+                if (e.Message.Equals("Invalid Format"))
+                    ViewData["Result"] = e.Message;
+                else
+                    ViewData["Result"] = "There has been a problem with the Application";                
+            }
+
+            return View();
+
+        }
+
+
+
+        /// <summary>
+        /// Validates and split the required string
+        /// </summary>
+        /// <param name="str">String to split</param>
+        /// <param name="splitter">Separator</param>
+        /// <param name="validNumber">Valid number of items</param>
+        /// <returns></returns>
+        private List<Rational> splitString(string str, int validNumber)
+        {
+            List<string> list = new List<string>();
+            List<Rational> rationals = new List<Rational>();
+            Rational rat;
+            long numerator, denominator;
+            int count = 0;
+
+            count = str.Split(' ').Count();
+
+            if(count != validNumber )
+                throw new Exception("Invalid Format");
+
+            else
+            {
+                foreach (string item in str.Split(' '))
+                {
+                    count = item.Split('/').Count();
+                    
+                    if (count != validNumber)
+                        throw new Exception("Invalid Format");
+                    else
+                    {
+                        long.TryParse(item.Split('/')[0], out numerator);
+                        long.TryParse(item.Split('/')[1], out denominator);
+                        rat = new Rational(numerator, denominator);
+                        rationals.Add(rat);
+                    }                                  
+                }                   
+            }
+           
+            return rationals;
+        }
+     
+       
+    }
+}
---
+++ Models/Rational.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RationalNumbers.Models
+{
+    public class Rational 
+    {
+        private long numerator;
+        private long denominator;
+
+        public Rational(long num, long den)
+        {        
+            numerator = num;
+            denominator = den;
+        }
+
+        public Rational add (Rational r) 
+        {
+            return new Rational(numerator * r.denominator + denominator * r.numerator, denominator * r.denominator);
+        }
+
+        public Rational sub(Rational r)
+        {
+            return new Rational(numerator * r.denominator - denominator * r.numerator, denominator * r.denominator);
+        }
+
+        public Rational mul(Rational r) 
+        {
+            return new Rational(numerator * r.numerator, denominator * r.denominator);
+        }
+
+        public Rational div (Rational r) 
+        {
+            return new Rational(numerator * r.denominator, denominator * r.numerator);
+        }
+
+        public string Simplify()
+        {
+            long remainder, result, gcd;
+            string str;
+
+            //If the denominator in the division is zero, output ""Inf""
+            if (this.denominator == 0)
+                return "Inf";
+        
+            result = Math.DivRem(this.numerator, this.denominator, out remainder);
+
+            //If it is possible to simplify the rational number, it does.
+            if (remainder != 0 && this.denominator != 0)
+            {
+                gcd = greatestCommonDivisor(Math.Abs(remainder), Math.Abs(this.denominator));
+                remainder = remainder / gcd;
+                this.denominator = this.denominator / gcd;
+            }            
+            
+            //if result is different from 0 it shows the integer part and does not show the negative sign from the rational part
+            if (result != 0)
+            {
+                //if remainder (numerator) is 0, it just shows the integer part
+                if (remainder == 0)
+                    str = result.ToString();
+                else
+                    str = result.ToString() + " " + Math.Abs(remainder).ToString() + "/" + Math.Abs(this.denominator).ToString();
+            }
+            else
+            { 
+                if (this.denominator < 0)
+                    str = "-" + Math.Abs(remainder).ToString() + "/" + Math.Abs(this.denominator).ToString();
+                else
+                    str = remainder.ToString() + "/" + this.denominator.ToString(); 
+            }
+                
+
+            //if the numerator is negative it close the result string with ()
+            if (this.numerator < 0 || this.denominator < 0)
+                str = "(" + str + ")";
+
+            return str;
+        }
+
+
+        private long greatestCommonDivisor(long a, long b)
+        {           
+            while (b > 0)
+            {
+                long rem = a % b;
+                a = b;
+                b = rem;
+            }
+            return a;
+        }   
+        
+    }
+
+}
